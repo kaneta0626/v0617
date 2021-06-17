234b367aba9be3aa1a1e239b7085ec9e84bbe960
A601-15 <tnk.dat.class@gmail.com>
2021-06-17 09:55:08 +0900

    Initial commit

---
+++ v0617/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0617
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int vx=-10;
+            int vy=-10;
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if(label1.Right>ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if(label1.Top < 0 )
+            {
+                vy = Math.Abs(vy);
+            }
+            if(label1.Bottom>ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+         
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            bool enabled = timer1.Enabled;
+        }
+    }
+}
